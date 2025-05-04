8405c8df54f50ed7690388c6c4b7acd523c5d62b
freehit154 <animoj77@gmail.com>
2025-05-04 16:19:28 +0530

    Update love.js

--- file/love.js
+++ file/love.js
@@ -169,7 +169,7 @@
             ctx.scale(0.75, 0.75);
             ctx.font = "12px,Verdana"; // 字号肿么没有用? (ˉ(∞)ˉ)
             ctx.fillText("Click Kr Gandu:) ", 30, -5);
-            ctx.fillText("Bta Kaisa Laga !", 28, 10);
+            ctx.fillText("Bta Kaisa Laga!", 28, 10);
             ctx.restore();
         },
         clear: function() {
